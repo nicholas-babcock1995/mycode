2bb1151f7258a91bc8488c932e10344a20951552
Nicholas Babcock <nicholas.babcock@tlgcohort.com>
2021-08-09 17:35:31 +0000

    Exploring how to work with large data sets

---
+++ iceAndFire04.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Alta3 Research - Exploring OpenAPIs with requests"""
+# documentation for this API is at
+# https://anapioficeandfire.com/Documentation
+
+import requests
+import pprint
+
+AOIF_CHAR = "https://www.anapioficeandfire.com/api/characters/"
+AOIF_HOUSES = "https://www.anapioficeandfire.com/api/houses/"
+def main():
+        ## Ask user for input
+        got_charToLookup = input("Pick a number between 1 and 1000 to return info on a GoT character! " )
+
+        ## Send HTTPS GET to the API of ICE and Fire character resource
+        gotresp = requests.get(AOIF_CHAR + got_charToLookup)
+
+        ## Decode the response
+        got_dj = gotresp.json()
+        pprint.pprint(got_dj)
+
+        print("------------------------------------------------------------------")
+        print("CODE CUSTOMIZATION")
+
+        #i think we should store the requests for the house information seperatel
+        print("Associated HOUSES!")
+        if 'allegiances' in got_dj:
+            for allegiance in got_dj['allegiances']:
+                houseresp = requests.get(allegiance)
+                house_json = houseresp.json()
+                if 'name' in house_json:
+                    print(house_json['name'])
+        print("Associated Books!")
+        if 'books' in got_dj:
+            for book in got_dj['books']:
+                bookresp = requests.get(book)
+                book_json = bookresp.json()
+                if 'name' in book_json:
+                    print(book_json['name'])
+if __name__ == "__main__":
+        main()
+
