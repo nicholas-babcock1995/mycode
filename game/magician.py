53f3b3f6e3e5558749d74316a1236db2f10c30e7
Nicholas Babcock <nicholas.babcock@tlgcohort.com>
2021-08-10 20:00:07 +0000

    Jinja templating for HTML and configs

--- game/magician.py
+++ game/magician.py
@@ -3,11 +3,11 @@ import sys, pygame
 import os
 os.environ["SDL_VIDEODRIVER"] = "dummy"
 pygame.init()
-pygame.display.init()
-
-
+background_colour = (255,255,255)
 gameDisplay = pygame.display.set_mode((800,600))
 pygame.display.set_caption('A bit Racey')
+gameDisplay.fill(background_colour)
+pygame.display.flip()
 clock = pygame.time.Clock()
 crashed = False
 
